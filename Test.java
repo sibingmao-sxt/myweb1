8a101259df9daa99e366767c6916f2c652ee80be
sibingmao <775090843@qq.com>
2020-12-11 10:53:08 +0800

    修改了Test.java，增加了变量i,j,z。

--- Test.java
+++ Test.java
@@ -2,6 +2,9 @@ public class Test{
 	//dev分支增加的注释
     public static void main(String[] args){
         System.out.println("Hello");
+	int i=90,j=80;
+	int z=j-i;
+	System.out.println("z="+z);
 	System.out.println("master分支增加了一行.");
     }
 }
